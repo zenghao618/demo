5bce892008ac8fb257b37107e4bf177f17f361d1
zenghao618 <56208790+zenghao618@users.noreply.github.com>
2019-10-28 13:14:23 +0800

    Initial commit

---
+++ druid/src/main/java/com/zenghao/demo/druid/conf/DruidConfig.java
@@ -0,0 +1,73 @@
+package com.zenghao.demo.druid.conf;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+/**
+ * ClassName: DruidConfig
+ * Description: Druid连接池的配置类
+ * date: 2019/10/28 11:54
+ *
+ * @author 小皓皓
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@Configuration
+@PropertySource(value = "classpath:application.yml")
+public class DruidConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    /**
+     * 注册一个StatViewServlet
+     * @return 将StatViewServlet以Bean形式注册到Spring容器中
+     */
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidStatViewServlet(){
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(),"/druid/*");
+
+        //添加初始化参数：initParams
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny","192.168.1.73");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","root");
+        servletRegistrationBean.addInitParameter("loginPassword","root");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return 注册的filterRegistrationBean
+     */
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> druidStatFilter(){
+
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
